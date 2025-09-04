176f7e913c65e3f4280ff3a17725cfa0e3e4b30f
HeRa Khan <heraemt@gmail.com>
2025-09-04 22:36:03 +0600

    1

--- app/(root)/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Settings } from "@/lib/Settings"
-
-export default async function HomePage() {
-  const settings = await Settings()
-
-  return <div>{settings.site || "Welcome to CMS"}</div>
-}
--- models/settings.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from 'mongodb';
-
-export interface Setting {
-  _id: ObjectId;
-  name: string;
-  content: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
